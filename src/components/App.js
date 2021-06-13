d76927173394cbe8916646bcc741c0b609ef4e52
gitAmelio <ameliosconnection@gmail.com>
2021-06-13 16:55:21 +0200

    React Basics

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Header from './Header'; 
+
+class App extends React.Component {
+
+    state = { 
+        pageHeader: 'Naming Contests'
+    };
+
+    render() {
+        return  (
+            <div>
+                <Header message={this.state.pageHeader}/>
+                <div>
+                    {this.state.test}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default App;
